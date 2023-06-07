096636263236964f4faf026d8d775b0964235738
Luan Fonseca De Lima <128102163+luanzin19@users.noreply.github.com>
2023-06-07 20:40:10 -0300

    Create cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,24 @@
+<?php
+ 
+?>
+<html>
+<center>
+        <h1>CADASTRAR</h1>
+
+    <form id="login" action="logado.php" method="POST">
+            Login: <input type="text" name="login"> <br>
+            Senha: <input type="password" name="senha" ><br><br>
+            Email: <input type="text" name="Email"><br>
+            <input type="submit" id="" value="Criar">
+        </form>
+    </center>    
+</html>
+<html>
+<head><style>
+body{
+background-image:linear-gradient(45deg, cyan, yellow);
+}
+</style></head>
+<body>
+</body>
+</html>
